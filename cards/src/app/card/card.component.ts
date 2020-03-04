62383bb731c86d6fd3b5d7d9e86af428fd95157a
eonsubae <eonsubae@naver.com>
2020-03-04 19:16:22 +0900

    cards

---
+++ cards/src/app/card/card.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from "@angular/core";
+
+@Component({
+  selector: "app-card",
+  templateUrl: "./card.component.html",
+  styleUrls: ["./card.component.css"]
+})
+export class CardComponent implements OnInit {
+  @Input() title: string;
+  @Input() imageUrl: string;
+  @Input() content: string;
+  @Input() username: string;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
