9b5d97d8ca61cc2979b26cd81a4302d8e84c9bc0
eonsubae <eonsubae@naver.com>
2020-03-06 06:09:44 +0900

    pages(directives)

---
+++ pages/src/app/class.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, ElementRef, Input } from "@angular/core";
+
+@Directive({
+  selector: "[appClass]"
+})
+export class ClassDirective {
+  constructor(private element: ElementRef) {}
+
+  @Input("appClass") set backgroundColor(classObj: any) {
+    for (let key in classObj) {
+      if (classObj[key]) {
+        this.element.nativeElement.classList.add(key);
+      } else {
+        this.element.nativeElement.classList.remove(key);
+      }
+    }
+  }
+}
